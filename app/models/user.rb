fa420d3d85f9edf92232b5792b021c078f032aa9
azizdevfull <aziz16110904@gmail.com>
2022-09-07 22:14:30 +0500

    fix some bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,16 +25,25 @@ class User < ApplicationRecord
          def self.find_for_database_authentication(warden_conditions)
           conditions = warden_conditions.dup
           if (login = conditions.delete(:login))
-            where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+            where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { 
+              value: login.strip.downcase 
+              }]).first
           elsif conditions.has_key?(:username) || conditions.has_key?(:email)
             where(conditions.to_h).first
           end
         end 
-        validates :username,
+  #       validates :username,
+  # :presence => true,
+  # :uniqueness => {
+  #   :case_sensitive => false
+  # }
+  validates :username, 
   :presence => true,
   :uniqueness => {
     :case_sensitive => false
-  }
+  },
+  format: { with: /\A[a-z0-9]+\z/ }
+
         def validate_username
           if User.where(email: username).exists?
             errors.add(:username, :invalid)
