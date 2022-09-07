2b88a13efcf255d63acae0e4bec0b22471d7c847
azizdevfull <aziz16110904@gmail.com>
2022-09-07 21:48:45 +0500

    fix some bugs usernamee's

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,24 @@ class ApplicationController < ActionController::Base
 
   protected
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:account_update, keys: [:username, :avatar, :website, :bio])
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :avatar])
-    end
+  def configure_permitted_parameters
+    added_attrs = [:username, :email, :password, :password_confirmation, :bio, :website]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: [:login, :password]
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
+
+
+
+  #   before_action :configure_permitted_parameters, if: :devise_controller?
+
+#   protected
+
+#     def configure_permitted_parameters
+#       devise_parameter_sanitizer.permit(:account_update, keys: [:username, :avatar, :website, :bio])
+#       devise_parameter_sanitizer.permit :sign_in, keys: [:login, :password]
+#       devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :avatar])
+#     end
+# end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,37 +1,54 @@
 class User < ApplicationRecord
   extend FriendlyId
    friendly_id :username, use: :slugged 
- 
+   has_many :posts
+   has_many :comments
+   has_many :likes
+   has_many :bookmarks
+   has_one_attached :avatar
+
    def should_generate_new_friendly_id?
     slug.blank? || username_changed?
   end
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :posts
-  has_many :comments
-  has_many :likes
-  has_many :bookmarks
-  has_one_attached :avatar
+         attr_writer :login
+         validate :validate_username
 
-    # This access the Relationship object.
+         def login
+           @login || self.username || self.email
+         end
+   
+         def self.find_for_database_authentication(warden_conditions)
+          conditions = warden_conditions.dup
+          if (login = conditions.delete(:login))
+            where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+          elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+            where(conditions.to_h).first
+          end
+        end 
+        
+        def validate_username
+          if User.where(email: username).exists?
+            errors.add(:username, :invalid)
+          end
+        end
+
     has_many :followed_users,
     foreign_key: :follower_id,
     class_name: 'Relationship',
     dependent: :destroy
 
-# This accesses the user through the relationship object.
 has_many :followees, through: :followed_users, dependent: :destroy
 
-# This access the Relationship object.
 has_many :following_users,
     foreign_key: :followee_id,
     class_name: 'Relationship',
     dependent: :destroy
 
-# This accesses the user through the relationship object.
 has_many :followers, through: :following_users, dependent: :destroy
 
   after_commit :add_default_avatar, on: %i[create update]
@@ -57,18 +74,4 @@ has_many :followers, through: :following_users, dependent: :destroy
   end
   def self.search(q)
   end
-  # scope :filter_by_username, -> (username) { where('username LIKE ?', "%#{username}%") }
-
-  # def self.search(params)
-  #   params[:query].blank? ? all : where(
-  #     "name LIKE ?", "%#{sanitize_sql_like(params[:query])}%"
-  #   )
-  # end
-  # def self.search(term)
-  #   if term
-  #     where('username LIKE ?', "%#{term}%")
-  #   else
-  #     nil
-  #   end
-  # end
 end
