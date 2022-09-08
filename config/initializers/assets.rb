7f93a31b8c06ae56a7077406118dce3e1071ade3
azizdevfull <aziz16110904@gmail.com>
2022-09-08 20:36:54 +0500

    added frontent to user authentication page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,8 @@ Rails.application.config.assets.version = "1.0"
 Rails.application.config.assets.precompile += %w( profile.css )
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.precompile += %w( signup.css )
+Rails.application.config.assets.precompile += %w( signin.css )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
